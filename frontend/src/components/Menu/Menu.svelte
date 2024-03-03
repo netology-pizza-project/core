8a3f544a67e1644ea5a17aac46b1d0d3988d764e
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-03-03 13:29:38 +0300

    header is ready

---
+++ frontend/src/components/Menu/Menu.svelte
@@ -0,0 +1,16 @@
+<nav>
+	<ul>
+		<li>item 1</li>
+		<li>item 2</li>
+		<li>item 3</li>
+		<li>item 4</li>
+		<li>item 5</li>
+		<li>item 6</li>
+		<li>item 7</li>
+		<li>item 8</li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	@import 'Menu.scss';
+</style>
