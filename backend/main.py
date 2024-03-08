6e6217c155dd4c47aa61fa36f2a81d101dd05ad7
aleksei.mikheev <stud0000298869@study.utmn.ru>
2024-03-08 20:44:19 +0300

    added root end point

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,40 @@
-from fastapi import FastAPI
+import os
+import uuid
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse, FileResponse
+from sqlalchemy import create_engine
+import psycopg2
+
+
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="<PASSWORD>", host="localhost", port="5432")
+
+cursor = conn.cursor()
+cursor.execute("""SELECT 
+                    product_id, 
+                    product_title, 
+                    product_description, 
+                    product_image, 
+                    product_is_new, 
+                    product_price 
+                FROM products
+                WHERE product_available is TRUE""")
+
+print(cursor.fetchall())
+product_list = cursor.fetchall()
+
+cursor.close()
+conn.close()
+
 
 app = FastAPI()
 
 
 @app.get("/")
+# цикл, который собирает из БД все пиццы и возвращает json
 async def root():
-    return {"message": "Hello World"}
+
+    return {"message": "Hello World"
+            }
 
 
 @app.get("/hello/{name}")
