3453397e712ea9d5b06b74a14924e6b11aefb1a5
NELETOOOO <lera-zima15@mail.ru>
2024-03-09 20:32:09 +0500

    added menu navigation

---
+++ frontend/src/components/Menu/Menu.svelte
@@ -0,0 +1,40 @@
+<nav>
+	<ul>
+		<li>
+			<a href="#">Пицца</a>
+		</li>
+		<li>
+			<a href="#">Паста</a>
+		</li>
+		<li>
+			<a href="#">Супы</a>
+		</li>
+		<li>
+			<a href="#">Салаты</a>
+		</li>
+		<li>
+			<a href="#">Напитки</a>
+		</li>
+		<li>
+			<a href="#">Десерты</a>
+		</li>
+		<li>
+			<a href="#">Бакалея</a>
+		</li>
+		<li>
+			<a href="#">Акции</a>
+		</li>
+		<li>
+			<a href="#">Комбо</a>
+		</li>
+		<li>
+			<a href="#">Контакты</a>
+		</li>
+	</ul>
+
+	<button>Корзина <span>1</span></button>
+</nav>
+
+<style lang="scss">
+ 	@import "Menu.scss"
+</style>
