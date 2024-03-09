170cdded0afb9284d2b5b47d76cff9081cd3445a
aleksei.mikheev <stud0000298869@study.utmn.ru>
2024-03-10 00:12:15 +0300

    Добавил return в виде json структуры для root()

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
+import json
 import os
 import uuid
 from fastapi import FastAPI, Body, status
 from fastapi.responses import JSONResponse, FileResponse
-from sqlalchemy import create_engine
 import psycopg2
 
 
@@ -19,22 +19,29 @@ cursor.execute("""SELECT
                 FROM products
                 WHERE product_available is TRUE""")
 
-print(cursor.fetchall())
+
 product_list = cursor.fetchall()
+print(product_list)
 
 cursor.close()
 conn.close()
+
+result = {}
+for i in product_list:
+    result[i[0]] = {"product_title": i[1],
+                    "product_description": i[2],
+                    "product_image": i[3],
+                    "product_is_new": i[4],
+                    "product_price": i[5]}
 
 
 app = FastAPI()
 
 
 @app.get("/")
-# цикл, который собирает из БД все пиццы и возвращает json
 async def root():
 
-    return {"message": "Hello World"
-            }
+    return json.dumps(result, ensure_ascii=False)
 
 
 @app.get("/hello/{name}")
