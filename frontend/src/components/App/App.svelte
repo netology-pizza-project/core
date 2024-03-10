d5cf98c5a42dff61fcc71ae49b3fb31d83213eaf
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-03-10 19:59:17 +0300

    updated menu

--- frontend/src/components/App/App.svelte
+++ frontend/src/components/App/App.svelte
@@ -1,11 +1,16 @@
 <script>
 	import Container from '../Container/Container.svelte';
 	import Header from '../Header/Header.svelte';
+	import Menu from '../Menu/Menu.svelte';
 </script>
 
 <main>
 	<Container>
 		<Header />
+	</Container>
+
+	<Container>
+		<Menu />
 	</Container>
 </main>
 
