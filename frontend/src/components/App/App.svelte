7418936e49a5aeea5257c79d803fbc7bf49c8b5e
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-03-19 21:49:14 +0300

    styled btns

--- frontend/src/components/App/App.svelte
+++ frontend/src/components/App/App.svelte
@@ -3,14 +3,6 @@
 	import Header from '../Header/Header.svelte';
 	import Menu from '../Menu/Menu.svelte';
 	import Slider from '../Slider/Slider.svelte';
-
-	// TODO: делать запрос за картинками
-	const images = [
-		{ id: '0', path: 'src/assets/slider/1.png' },
-		{ id: '1', path: 'src/assets/slider/2.png' },
-		{ id: '2', path: 'src/assets/slider/3.png' },
-		{ id: '3', path: 'src/assets/slider/4.png' }
-	];
 </script>
 
 <main>
@@ -22,18 +14,7 @@
 		<Menu />
 	</Container>
 
-	<Container>
-		<Slider
-			{images}
-			imageWidth={250}
-			imageSpacing={15}
-			controlColor={'black'}
-			controlScale={'0.8'}
-			displayControls={true}
-			autoplay={true}
-			autoplaySpeed={3000}
-		/>
-	</Container>
+	<Slider />
 </main>
 
 <style lang="scss">
--- frontend/src/components/Slider/Slider.svelte
+++ frontend/src/components/Slider/Slider.svelte
@@ -3,51 +3,55 @@
 	import { onMount, onDestroy } from 'svelte';
 	import type { IImages } from './types';
 
-	export let images: IImages[];
-	export let imageWidth = 300;
-	export let imageSpacing = 20;
-	export let speed = 500;
-	export let controlColor = '#444';
-	export let controlScale = '0.5';
-	export let autoplay = false;
-	export let autoplaySpeed = 5000;
-	export let displayControls = true;
+	// TODO: делать запрос за картинками
+	let images: IImages[] = [
+		{ id: '0', path: 'src/assets/slider/1.png' },
+		{ id: '1', path: 'src/assets/slider/2.png' },
+		{ id: '2', path: 'src/assets/slider/3.png' },
+		{ id: '3', path: 'src/assets/slider/4.png' }
+	];
+	const speed = 500;
+
+	const autoplay = true;
+	const autoplaySpeed = 2000;
 
 	let interval: ReturnType<typeof setInterval>;
 
-	const updateOpacity = (id: string, opacity: string) => {
+	function updateOpacity(id: string, opacity: string) {
 		const imageElement = document.getElementById(String(id));
 
 		if (imageElement) {
 			imageElement.style.opacity = opacity;
 		}
-	};
+	}
 
-	const rotateLeft = () => {
+	function rotateLeft() {
 		const transitioningImage = images[images.length - 1];
 
 		updateOpacity(transitioningImage.id, '0');
 		images = [images[images.length - 1], ...images.slice(0, images.length - 1)];
 
 		setTimeout(() => updateOpacity(transitioningImage.id, '1'), speed);
-	};
+	}
 
-	const rotateRight = () => {
+	function rotateRight() {
 		const transitioningImage = images[0];
 
 		updateOpacity(transitioningImage.id, '0');
 		images = [...images.slice(1, images.length), images[0]];
-		
-		setTimeout(() => updateOpacity(transitioningImage.id, '1'), speed);
-	};
 
-	const startAutoPlay = () => {
+		setTimeout(() => updateOpacity(transitioningImage.id, '1'), speed);
+	}
+
+	function startAutoPlay() {
 		if (autoplay) {
 			interval = setInterval(rotateLeft, autoplaySpeed);
 		}
-	};
+	}
 
-	const stopAutoPlay = () => clearInterval(interval);
+	function stopAutoPlay() {
+		clearInterval(interval);
+	}
 
 	onMount(() => {
 		if (autoplay) {
@@ -58,45 +62,35 @@
 	onDestroy(stopAutoPlay);
 </script>
 
-<div id="carousel-container">
-	<div id="carousel-images">
-		{#each images as image (image.id)}
-			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-			<img
-				src={image.path}
-				alt={image.id}
-				id={image.id}
-				style={`width:${imageWidth}px; margin: 0 ${imageSpacing}px;`}
-				on:mouseover={stopAutoPlay}
-				on:mouseout={startAutoPlay}
-				animate:flip={{ duration: speed }}
-			/>
-		{/each}
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="carousel" on:mouseover={stopAutoPlay} on:mouseout={startAutoPlay}>
+	<div class="carousel__container">
+		<div class="carousel__images">
+			{#each images as image (image.id)}
+				<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+				<img src={image.path} alt="item" id={image.id} animate:flip={{ duration: speed }} />
+			{/each}
+		</div>
+		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+		<button class="carousel__btn" id="left" on:click={rotateLeft}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+				<path
+					d="M14.2893 5.70708C13.8988 5.31655 13.2657 5.31655 12.8751 5.70708L7.98768 10.5993C7.20729 11.3805 7.2076 12.6463 7.98837 13.427L12.8787 18.3174C13.2693 18.7079 13.9024 18.7079 14.293 18.3174C14.6835 17.9269 14.6835 17.2937 14.293 16.9032L10.1073 12.7175C9.71678 12.327 9.71678 11.6939 10.1073 11.3033L14.2893 7.12129C14.6799 6.73077 14.6799 6.0976 14.2893 5.70708Z"
+					fill="#0F0F0F"
+				/>
+			</svg>
+		</button>
+		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+		<button class="carousel__btn" id="right" on:click={rotateRight}>
+			<svg transform={'rotate(180)'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+				<path
+					d="M14.2893 5.70708C13.8988 5.31655 13.2657 5.31655 12.8751 5.70708L7.98768 10.5993C7.20729 11.3805 7.2076 12.6463 7.98837 13.427L12.8787 18.3174C13.2693 18.7079 13.9024 18.7079 14.293 18.3174C14.6835 17.9269 14.6835 17.2937 14.293 16.9032L10.1073 12.7175C9.71678 12.327 9.71678 11.6939 10.1073 11.3033L14.2893 7.12129C14.6799 6.73077 14.6799 6.0976 14.2893 5.70708Z"
+					fill="#0F0F0F"
+				/>
+			</svg>
+		</button>
 	</div>
-	{#if displayControls}
-		<button id="left" on:click={rotateLeft}>
-			<svg width="39px" height="110px" id="svg8" transform={`scale(${controlScale})`}>
-				<g id="layer1" transform="translate(-65.605611,-95.36949)">
-					<path
-						style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
-						d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
-						id="path1412"
-					/>
-				</g>
-			</svg>
-		</button>
-		<button id="right" on:click={rotateRight}>
-			<svg width="39px" height="110px" id="svg8" transform={`rotate(180) scale(${controlScale})`}>
-				<g id="layer1" transform="translate(-65.605611,-95.36949)">
-					<path
-						style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
-						d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
-						id="path1412"
-					/>
-				</g>
-			</svg>
-		</button>
-	{/if}
 </div>
 
 <style lang="scss">
