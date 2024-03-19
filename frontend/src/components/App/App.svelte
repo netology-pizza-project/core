f2ccedb6a7cf6f49cdf1a7126d3496ae5acd1dcf
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-03-19 18:13:15 +0300

    added structure of array

--- frontend/src/components/App/App.svelte
+++ frontend/src/components/App/App.svelte
@@ -2,6 +2,13 @@
 	import Container from '../Container/Container.svelte';
 	import Header from '../Header/Header.svelte';
 	import Menu from '../Menu/Menu.svelte';
+
+	let images = [
+		{ id: 0, path: 'src/assets/slider/1.png' },
+		{ id: 1, path: 'src/assets/slider/2.png' },
+		{ id: 2, path: 'src/assets/slider/3.png' },
+		{ id: 3, path: 'src/assets/slider/4.png' }
+	];
 </script>
 
 <main>
