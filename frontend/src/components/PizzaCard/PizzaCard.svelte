52ae73e73d149da129f06c0e3f135b0077da1177
Khrisanf <vikachris2007@gmail.com>
2024-03-25 22:14:26 +0500

    Вёрстка карточки пиццы

---
+++ frontend/src/components/PizzaCard/PizzaCard.svelte
@@ -0,0 +1,16 @@
+<h1 class="Title_Main">Паста</h1>
+<div class="content-container">
+	<img src="src/assets/special/spec_piz_1.png" alt="пицца" class="customImage" />
+	<h3 class="imageTitle">с креветками и трюфелями</h3>
+	<p class="description">
+		Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г
+	</p>
+	<div class="secondaryTitle_N_Button">
+		<h2 class="secondaryTitle">От 600р</h2>
+		<button class="customButton">В корзину</button>
+	</div>
+</div>
+
+<style lang="scss">
+	@import 'PizzaCard.scss';
+</style>
