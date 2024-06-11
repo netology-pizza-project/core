41c0a314d2f47c28578f46f82d26ec88d3dd1719
Aleksei Ermakov <epmak@pm.me>
2024-06-11 14:47:43 +0500

    final

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+
+from api.main import api_router
+from core.config import settings
+from db.base import Base, engine
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"/docs/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+
+@app.on_event("startup")
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+app.include_router(api_router)
